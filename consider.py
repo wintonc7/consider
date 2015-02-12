247c9e10c41c3c3da18e1c10a0b90cd5dc6321dd
Rohit Kapoor <rohit6301@gmail.com>
2015-02-12 17:39:00 -0500

    First change

--- consider.py
+++ consider.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
 	"""docstring for MainPage"""
 	def get(self):
-		self.response.write('Hello Rohit!')
+		self.response.write('Hello Rohit Kapoor!')
 
 
 application = webapp2.WSGIApplication([
