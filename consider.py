11e9f1b1dceece097923b8a442249b44a5c90358
Rohit Kapoor <rohit6301@gmail.com>
2015-02-12 20:35:35 -0500

    Demo sign in

--- consider.py
+++ consider.py
@@ -1,10 +1,35 @@
+import os
+import urllib
 import webapp2
+import jinja2
+
+from google.appengine.api import users
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
-	"""docstring for MainPage"""
+	"""Main function that will handle the first request"""
 	def get(self):
-		self.response.write('Hello Rohit Kapoor!')
 
+		user = users.get_current_user()
+
+		if user:
+			url = users.create_logout_url(self.request.uri)
+			template_values = {
+		    	'url': url
+			}
+			template = JINJA_ENVIRONMENT.get_template('home.html')
+			self.response.write(template.render(template_values))
+		else:
+			url = users.create_login_url(self.request.uri)
+			template_values = {
+		    	'url': url
+			}
+			template = JINJA_ENVIRONMENT.get_template('login.html')
+			self.response.write(template.render(template_values))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
