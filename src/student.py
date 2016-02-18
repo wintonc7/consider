fc21141abda845725bdc8b06a2f55342ad9a4d4f
Swaroop Joshi <joshi.127@osu.edu>
2016-02-18 03:46:28 -0500

    View improved - in student's view, only one topnavbar; has tabs for round1 to summary

--- src/student.py
+++ src/student.py
@@ -71,6 +71,7 @@ class Rounds(webapp2.RequestHandler):
                                 template_values['sectionKey'] = section_key
                                 if curr_round.number != 1:
                                     template_values['last_round'] = True
+                                template_values['rounds'] = curr_round.number
                                 template = utils.jinja_env().get_template('student_round.html')
                                 self.response.write(template.render(template_values))
                             else:
@@ -127,21 +128,21 @@ class Rounds(webapp2.RequestHandler):
                                 response.student = student.email
                                 response.put()
                                 utils.log(
-                                        'Your response have been saved. You can edit it any time before the deadline.',
-                                        type='S', handler=self)
+                                    'Your response have been saved. You can edit it any time before the deadline.',
+                                    type='S', handler=self)
                             else:
                                 utils.error(
-                                        'Sorry, the time for submission for this round has expired \
-                                           and your response was not saved, please wait for the next round.',
-                                        handler=self)
+                                    'Sorry, the time for submission for this round has expired \
+                                       and your response was not saved, please wait for the next round.',
+                                    handler=self)
                         else:
                             utils.error('Sorry! The round is not visible, please try again later.', handler=self)
                     else:
                         utils.error('Section is null', handler=self)
                 except:
                     utils.error(
-                            'Sorry! There was some error submitting your response please try again later.',
-                            handler=self)
+                        'Sorry! There was some error submitting your response please try again later.',
+                        handler=self)
             else:
                 utils.error('Invalid Parameters: section_key is null', handler=self)
         else:
@@ -149,7 +150,7 @@ class Rounds(webapp2.RequestHandler):
             self.redirect('/')
 
 
-class Discussion(webapp2.RequestHandler):  # FIXME Aliases mixed up.
+class Discussion(webapp2.RequestHandler):
     """
     API to retrieve discussion information, i.e. the responses from previous round and deadline for the current round.
     """
@@ -188,11 +189,11 @@ class Discussion(webapp2.RequestHandler):  # FIXME Aliases mixed up.
                                     group = models.Group.get_by_id(group, parent=section.key)
                                     if group:
                                         comments = []
-                                        if requested_round-1 == 0:
+                                        if requested_round - 1 == 0:
                                             previous_round = models.Round.get_by_id(1, parent=section.key)
                                         else:
                                             previous_round = models.Round.get_by_id(requested_round - 1,
-                                                                                parent=section.key)
+                                                                                    parent=section.key)
                                         for _student in group.members:
                                             response = models.Response.get_by_id(_student, parent=previous_round.key)
                                             if response:
@@ -219,7 +220,7 @@ class Discussion(webapp2.RequestHandler):  # FIXME Aliases mixed up.
                                         if stu_response:
                                             template_values['comment'] = stu_response.comment
                                             template_values['response'] = ','.join(
-                                                    str(item) for item in stu_response.response)
+                                                str(item) for item in stu_response.response)
                                         deadline = datetime.datetime.strptime(d_round.deadline,
                                                                               '%Y-%m-%dT%H:%M')
                                         current_time = datetime.datetime.now()
@@ -238,18 +239,18 @@ class Discussion(webapp2.RequestHandler):  # FIXME Aliases mixed up.
                                         self.response.write(template.render(template_values))
                                     else:
                                         utils.error(
-                                                'Group not found for {0} Section: {1}'.format(str(student), str(
-                                                        section)), handler=self)
+                                            'Group not found for {0} Section: {1}'.format(str(student), str(
+                                                section)), handler=self)
                                         self.redirect('/error?code=105')
                                 else:
                                     utils.error(
-                                            'Group not found for {0} Section: {1}'.format(str(student), str(section)),
-                                            handler=self)
+                                        'Group not found for {0} Section: {1}'.format(str(student), str(section)),
+                                        handler=self)
                                     self.redirect('/error?code=105')
                             else:
                                 utils.log(
-                                        'Requested round not found for {0} Section: {1}'.format(str(student), str(
-                                                section)))
+                                    'Requested round not found for {0} Section: {1}'.format(str(student), str(
+                                        section)))
                                 self.redirect('/home')
                     else:
                         utils.log('Section not found for key: ' + section_key)
@@ -317,9 +318,9 @@ class QuestionDisplay(webapp2.RequestHandler):
                             d_round = models.Round.get_by_id(requested_round, parent=section.key)
                             round_one = models.Round.get_by_id(1, parent=section.key)
                             template_values = {
-                                            'logouturl': logout_url,
-                                            'expired': True
-                                        }
+                                'logouturl': logout_url,
+                                'expired': True
+                            }
                             template_values['deadline'] = d_round.deadline
                             template_values['rounds'] = section.current_round
                             template_values['curr_page'] = requested_round
