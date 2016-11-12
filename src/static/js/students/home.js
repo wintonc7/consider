57cb3398f19129caa94ee193dd1ab2a451af6a1b
Cailin Pitt <cailinpitt1@gmail.com>
2016-11-12 17:47:16 -0500

    Added error code 106, which tells student they are not a part of a group and should contact instructor when they try to access round when they have not been assigned a group.

--- src/static/js/students/home.js
+++ src/static/js/students/home.js
@@ -1,7 +1,7 @@
 function redirect(key, group) {
   if (Number(group) === 0)
   {
-    location.href = '/error'
+    location.href = '/error?code=106'
   }
   else {
     location.href = '/student_rounds?section=' + key;
