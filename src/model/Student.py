5a010e8f1f54b45cad626a96420aab22531d7530
Alan Zeigler <zeigler.102@osu.edu>
2017-02-20 14:24:52 -0500

    Added additional fields for student profile page

--- src/model/Student.py
+++ src/model/Student.py
@@ -13,3 +13,11 @@ class Student(ndb.Model):
     """ String. Must be non-empty and unique. Retrieved from Google automatically """
     sections = ndb.KeyProperty(kind=Section, repeated=True, indexed=False)
     """ List of active `Section`_ s this student is enrolled in. """
+    preferred_emails = ndb.StringProperty()
+    """ Additional email addresses to notify. Configured through profile page. """
+    send_preferred = ndb.BooleanProperty(default=False)
+    """ True/false. Enables sending to alternate email. Configured through profile page. """
+    fname = ndb.StringProperty()
+    """ String. Optional. Configured through profile page. """
+    lname = ndb.StringProperty()
+    """ String. Optional. Configured through profile page. """
