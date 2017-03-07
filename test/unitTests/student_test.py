6f6d6b9a4b35b5fef540f781d60537f2b528616a
Edrienne <edrienneco@gmail.com>
2017-03-07 15:30:44 -0500

    Set up example tests, although student_test does not work

--- test/unitTests/student_test.py
+++ test/unitTests/student_test.py
@@ -1,8 +1,12 @@
 import unittest
 
-from google.appengine.api import memcache
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
+
 class TestStudent(ndb.Student):
     """A student class used for testing."""
+    email = ndb.StringProperty(default="dummy@gmail.com")
+    preferred_email = ndb.StringProperty()
+    fname = ndb.StringProperty()
+    lname = ndb.StringProperty()
