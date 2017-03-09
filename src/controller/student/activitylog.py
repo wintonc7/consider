fab26bf93f2a1a48206ec62e0ec2f8e38d70df78
Alan Zeigler <zeigler.102@osu.edu>
2017-03-09 17:20:57 -0500

    Added JSON export for log entries in Activity Log controller

--- src/controller/student/activitylog.py
+++ src/controller/student/activitylog.py
@@ -11,7 +11,7 @@ Implements the APIs for the Student activity log in the app.
 
 """
 
-import webapp2
+import webapp2, json, StringIO
 from google.appengine.api import users
 
 from src import model, utils
@@ -31,8 +31,32 @@ class ActivityLog(webapp2.RequestHandler):
         # end
 
         # Use obtained user object to obtain relevant log entries
+        output = {"logs": []}
+        groups = []
+        for section_key in student.sections:
+            groups.extend(model.Group.query(ancestor=section_key).fetch())
+        logs = []
+        for group in groups:
+            logs.extend(model.ActivityLog.query(ancestor=group.key).fetch())
+        log_entries = []
+        for log in logs:
+            log_entries.extend(model.LogEntry.query(ancestor=log.key).fetch())
+            new_dict = {
+                "course": log.course.get().name,
+                "assignment": log.assignment.get().name,
+                "entries": []
+            }
+            for log_entry in log_entries:
+                new_dict['entries'].append({
+                    "student": log_entry.student,
+                    "description": log_entry.description,
+                    "time": log_entry.time.strftime("%c")
+                })
+            output['logs'].append(new_dict)
 
         # Format log entries as JSON and export
+        output_string = json.dumps(output)
+        self.response.headers.add_header("Logs", output_string)
 
         # Create a url for the user to logout
         logout_url = users.create_logout_url(self.request.uri)
