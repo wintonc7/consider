1a1f6368cce656fb267567539ed7d8b416bd018b
Daniel Stelson <d.stelson@gmail.com>
2017-03-21 22:56:18 -0400

    HTML email confirmation edit
    adjusted email confirmation to use html formatting

--- src/controller/student/rounds.py
+++ src/controller/student/rounds.py
@@ -418,11 +418,13 @@ class Rounds(webapp2.RequestHandler):
             #send email confirmation
             to_addr = []
             to_addr.append(student.email)
-            msg = "You have submitted the following response: \n" + str(response.comment) + "\nYou have until " + str(deadline) + " to modify this submission \n\nThanks!\nThe CONSIDER Team"
+            #msg = "You have submitted the following response: \n" + str(response.comment) + "\nYou have until " + str(deadline) + " to modify this submission \n\nThanks!\nThe CONSIDER Team" plain text message
+            #html message
+            msg = "<html><body>Hello " + student.fname + " " + student.lname + ",<br><br>You have submitted the following response:<br><br><b>" + str(response.comment) + "</b><br><br>You have until <b>" + str(deadline) + "</b> to modify this submission<br><br>Thanks!<br>The CONSIDER Team</body></html>"
             current_section = current_round.key.parent().get()
             subject = "CONSIDER ASSIGNMENT " + str(current_section.name) + " SUBMISSION FOR ROUND " + str(current_round.number)
             utils.send_mails(to_addr, subject, msg)
-            utils.log("RESPONSE Email confirmation sent", type='Success!', handler=self)
+            utils.log("Email confirmation sent", type='Success!', handler=self)
         else:  # Otherwise alert them that time has passed to submit for this round
             utils.error('Sorry, the time for submission for this round has expired \
            and your response was not saved, please wait for the next round.',
