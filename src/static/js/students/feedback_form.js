68fb7fc41fade19ed152e6289c475454bcccead8
Daniel Stelson <d.stelson@gmail.com>
2017-04-04 23:04:38 -0400

    CKEditor incorporated into feedback
    Added ckeditor to feedback form. email broken but needs to be changed to
    anonymous anyways

--- src/static/js/students/feedback_form.js
+++ src/static/js/students/feedback_form.js
@@ -3,8 +3,29 @@ $(document).ready(function() {
   $("#feedback").submit(formSubmit);
 
   function formSubmit(event) {
+      //Prevent the default behaviour
+      event.preventDefault();
 
+      var email, tag, othertag, comments;
+
+      email = $('#email').value;
+      tag = $('#tag').val();
+      othertag= $('#othertag').val();
+
+      // Update content of textarea(s) handled by CKEditor
+      for ( instance in CKEDITOR.instances ) {
+          CKEDITOR.instances[instance].updateElement();
+          //console.log("CKEDITOR Instance FOUND");
+          //console.log("CKEditor contains " + CKEDITOR.instances[instance].getData());
+          comments = CKEDITOR.instances[instance].getData().replace(/<p>/g,'').replace(/<\/p>/g,'').replace(/&nbsp;/g,'');
+      }
+
+      var $form = $(this);
+      var url = $form.attr("action");
+
+          $.post(url, {email: email, tag: tag, othertag: othertag, comments: comments});
   }
+
 
   function CheckTag(val){
         var element = getElementById('othertag');
