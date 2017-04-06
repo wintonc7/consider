2f3698426993c828d638fdd41b2b27af008faf68
Daniel Stelson <d.stelson@gmail.com>
2017-04-06 19:10:14 -0400

    Implemented admin feedback viewing
    Admin can now view all feedback entities in the datastore. next step is
    to add tag filtering.

--- src/controller/feedbackviewer.py
+++ src/controller/feedbackviewer.py
@@ -20,10 +20,15 @@ class ViewFeedBackPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         logout_url = users.create_logout_url(self.request.uri)
+
+        #get feedback
+        feedback = model.Feedback.query()
+
         from src import config
         template_values = {
             'documentation': config.DOCUMENTATION,
-            'logouturl': logout_url
+            'logouturl': logout_url,
+            'feedback' : feedback
         }
         # Set the template html page
         template = utils.jinja_env().get_template('view_feedback.html')
