48841891b8423d1d289f2c9dbbfc7a90b44b123c
Alan Zeigler <zeigler.102@osu.edu>
2017-04-07 12:30:55 -0400

    Minor tweaks to course table display

--- src/static/js/students/home.js
+++ src/static/js/students/home.js
@@ -10,10 +10,14 @@ function redirect(key, group, round) {
 
 function collapse(id) {
   var card = document.getElementById(id);
-  if (card.hasChildNodes() && card.children[1].style.display == 'none') {
-        card.children[1].style.display = 'block';
-    } else {
-        card.children[1].style.display = 'none';
-    }
-  // TODO: ALLOW SHOWING AGAIN
+  // if (card.hasChildNodes() && card.children[1].style.display == 'none') {
+  //     card.children[1].style.display = 'block';
+  // } else {
+  //     card.children[1].style.display = 'none';
+  // }
+  if (card.hasChildNodes() && card.className == 'card course-card') {
+      card.className = 'card course-card course-card-collapsed';
+  } else {
+      card.className = 'card course-card';
+  }
 }
